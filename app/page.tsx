827b3eab5bf8e029b845dfa436ab1ae764adbeb9
Samuel Ward <samuelleeward9@gmail.com>
2025-08-21 11:42:04 +0200

    some styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,24 @@ export default async function Home() {
             <h1 className="text-2xl font-bold">Welcome to the Chat App</h1>
             <p className="mt-4">This is a simple chat application built with Next.js.</p>
             <Button className="mt-6 w-100">This is a button</Button>
-            <div className="mt-4">
-                <h2 className="text-xl font-semibold">Messages:</h2>
-                <ul className="list-disc pl-5">
+            <div className="mt-6">
+                <h2 className="text-xl font-semibold mb-2">Messages:</h2>
+                <ul className="list-none">
                     {messages.map((message) => (
-                        <li className={message.sent_by !== "samuelward" ? "bg-accent" : undefined} key={message.id}>
-                            {message.content}
+                        <li key={message.id}>
+                            <p className="text-xs mb-1">
+                                {message.created_at.toLocaleDateString() +
+                                    " / " +
+                                    message.created_at.toLocaleTimeString()}
+                            </p>
+                            <div
+                                className={
+                                    (message.sent_by !== "samuelward" ? "bg-accent" : undefined) +
+                                    " p-2 rounded-xl mb-4 inset-shadow-sm/8 shadow-lg/8"
+                                }
+                            >
+                                {message.sent_by + " - " + message.content}
+                            </div>
                         </li>
                     ))}
                 </ul>
