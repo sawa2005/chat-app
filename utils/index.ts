973de0184746e9f92c3c4c9c58e361f3cd68b998
Samuel Ward <samuelleeward9@gmail.com>
2025-11-10 13:19:21 +0100

    header classes refactored & moved to utils

--- utils/index.ts
+++ utils/index.ts
@@ -9,16 +9,3 @@ export function isValidUUID(id: string): boolean {
     const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
     return regex.test(id);
 }
-
-export function isOldMsg(msgDate: Date) {
-    const currentDate = new Date();
-
-    const milliDiff = currentDate.getTime() - msgDate.getTime();
-    const hoursDiff = Math.floor(milliDiff / (1000 * 60 * 60));
-
-    if (hoursDiff >= 24) {
-        return true;
-    } else {
-        return false;
-    }
-}
--- utils/messages/index.ts
+++ utils/messages/index.ts
@@ -1,5 +1,6 @@
 import { Message } from "@/lib/types";
 import emojiRegex from "emoji-regex";
+import { cn } from "..";
 
 export function isConsecutiveMessage(prev: Message | undefined, current: Message, cutoffMinutes = 5) {
     if (!prev) return false;
@@ -18,4 +19,25 @@ export function isEmojiOnly(message: string) {
     const stripped = message.replace(/\s/g, "");
     const matched = stripped.match(regex);
     return matched !== null && matched.join("") === stripped;
+}
+
+export function isOldMessage(msgDate: Date) {
+    const currentDate = new Date();
+
+    const milliDiff = currentDate.getTime() - msgDate.getTime();
+    const hoursDiff = Math.floor(milliDiff / (1000 * 60 * 60));
+
+    if (hoursDiff >= 24) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export function getMessageHeaderClasses(isOwner: boolean, isConsecutive: boolean, showActions: boolean) {
+    const baseClasses = "justify-end text-xs mb-1 flex items-center gap-2";
+    const ownerClasses = isOwner ? "text-right" : "flex-row-reverse";
+    const consecutiveClasses = isConsecutive ? (showActions ? "flex" : "hidden") : "mt-5";
+
+    return cn(baseClasses, ownerClasses, consecutiveClasses);
 }
