9e3f0efc70f5099ed5c980d516ede6156e0cf8d4
Samuel Ward <samuelleeward9@gmail.com>
2025-11-25 14:26:38 +0100

    fix: remove auth references in migration & proxy url fixed

--- lib/proxy.ts
+++ lib/proxy.ts
@@ -41,7 +41,7 @@ export async function updateSession(request: NextRequest) {
     if (!user && !request.nextUrl.pathname.startsWith("/login") && !request.nextUrl.pathname.startsWith("/auth")) {
         // no user, potentially respond by redirecting the user to the login page
         const url = request.nextUrl.clone();
-        url.pathname = "/auth/login";
+        url.pathname = "/login";
         return NextResponse.redirect(url);
     }
 
